7f8ad2f1c1000507041548b0182f9435dda53637
alisson <alissoneatena1@gmail.com>
2022-09-01 23:16:15 -0300

    create migration CreateProduct

---
+++ src/shared/http/server.ts
@@ -0,0 +1,31 @@
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import { router } from './routes';
+import { AppError } from './AppError';
+
+import '@shared/typeorm';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      message: err.message,
+    });
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error',
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000!');
+});
