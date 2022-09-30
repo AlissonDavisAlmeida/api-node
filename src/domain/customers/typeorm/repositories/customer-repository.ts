98ede88504285d371b548174347dfc7f329621e1
alisson <alissoneatena1@gmail.com>
2022-09-29 21:35:28 -0300

    add customer repository

---
+++ src/domain/customers/typeorm/repositories/customer-repository.ts
@@ -0,0 +1,20 @@
+import { EntityRepository, Repository } from "typeorm";
+import { Customer } from "../entities/Customer";
+
+@EntityRepository(Customer)
+export class CustomerRepository extends Repository<Customer>{
+
+    async findByName(name: string): Promise<Customer | undefined> {
+        const customer = await this.findOne({
+            where: { name }
+        });
+
+        return customer;
+    }
+
+    async findById(id: string): Promise<Customer | undefined> {
+        const customer = await this.findOne(id);
+
+        return customer;
+    }
+}
